a8174aeec862456e618dfab48b7982da44c42715
user <user@usernoMacBook-Pro.local>
2024-12-11 23:18:41 +0700

    Implement Returns & Cancellation Policy and Terms & Conditions

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -4,24 +4,46 @@ import { PrivacyPolicyContent } from "@/constants";
 const PrivacyPolicyPage: FC = () => {
   return (
     <div className="container mx-auto">
-      <div className="flex justify-between lg:flex-row flex-col pt-[100px] xl:pt-[133px] pb-[50px] items-center">
-        <article className="max-w-[812px] m-auto">
-          <h1 className="text-[40px] xl:text-[64px] leading-tight font-bold text-[#0a1015] mb-6">
-            {PrivacyPolicyContent.title}
-          </h1> 
+      <div className="flex justify-center lg:flex-row flex-col pb-[50px] items-center">
+        <article>
+          <div className="text-center py-[30px] sm:py-[40px] lg:py-[60px] xl:py-[80px]">
+            <div className="w-full px-[15px] sm:px-[20px] lg:px-[30px] max-w-[940px] mx-auto">
+              <h1 className="w-full text-[#110d34] mb-[10px] text-[36px] sm:text-[45px] lg:text-[50px] xl:text-[55px] font-bold leading-[1.1em] font-generalsans">
+                {PrivacyPolicyContent.title}
+              </h1>
+            </div>
+          </div>
+          <div className="pb-[50px] md:pb-[70px] lg:pb-[80px]">
+            <div className="max-w-[1400px] px-[20px] md:px-[30px] lg:max-w-[940px] mx-auto">
+              <div className="bg-[#faf5ff] rounded-[30px] p-[20px]">
+                <h2 className="text-[#110d34] mt-0 mb-[10px] text-[21px] md:text-[25px] lg:text-[28px] font-bold leading-[1.1em]">
+                  {PrivacyPolicyContent.subtitle}
+                </h2>
 
-          <h2 className="text-[#0a1015] text-[16px] leading-[1.5] font-normal mb-[30px]">
-            {PrivacyPolicyContent.subtitle}
-          </h2>
+                {PrivacyPolicyContent.content.map((paragraph, index) => (
+                  <p
+                    key={index}
+                    className="text-[#0a1015] text-[16px] leading-[1.5] font-normal mb-[30px]"
+                  >
+                    {paragraph}
+                  </p>
+                ))}
 
-          {PrivacyPolicyContent.content.map((paragraph, index) => (
-            <p
-              key={index}
-              className="text-[#0a1015] text-[16px] leading-[1.5] font-normal mb-[30px]"
-            >
-              {paragraph}
-            </p>
-          ))}
+                <h2 className="text-[#110d34] mt-0 mb-[10px] text-[21px] md:text-[25px] lg:text-[28px] font-bold leading-[1.1em]">
+                  {PrivacyPolicyContent.subTitle2}
+                </h2>
+
+                {PrivacyPolicyContent.content2.map((paragraph, index) => (
+                  <p
+                    key={index}
+                    className="text-[#0a1015] text-[16px] leading-[1.5] font-normal mb-[30px]"
+                  >
+                    {paragraph}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
         </article>
       </div>
     </div>
--- src/app/term-and-conditions/page.tsx
+++ src/app/term-and-conditions/page.tsx
@@ -1,3 +1,50 @@
+import { TermsAndConditionsContent } from "@/constants";
+
 export default function TermAndConditionsPage() {
-  return <div>TermAndConditions</div>;
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center lg:flex-row flex-col pb-[50px] items-center">
+        <article>
+          <div className="text-center py-[30px] sm:py-[40px] lg:py-[60px] xl:py-[80px]">
+            <div className="w-full px-[15px] sm:px-[20px] lg:px-[30px] max-w-[940px] mx-auto">
+              <h1 className="w-full text-[#110d34] mb-[10px] text-[36px] sm:text-[45px] lg:text-[50px] xl:text-[55px] font-bold leading-[1.1em] font-generalsans">
+                {TermsAndConditionsContent.title}
+              </h1>
+            </div>
+          </div>
+          <div className="pb-[50px] md:pb-[70px] lg:pb-[80px]">
+            <div className="max-w-[1400px] px-[20px] md:px-[30px] lg:max-w-[940px] mx-auto">
+              <div className="bg-[#faf5ff] rounded-[30px] p-[20px]">
+                <h2 className="text-[#110d34] mt-0 mb-[10px] text-[21px] md:text-[25px] lg:text-[28px] font-bold leading-[1.1em]">
+                  {TermsAndConditionsContent.subtitle}
+                </h2>
+
+                {TermsAndConditionsContent.content.map((paragraph, index) => (
+                  <p
+                    key={index}
+                    className="text-[#0a1015] text-[16px] leading-[1.5] font-normal mb-[30px]"
+                  >
+                    {paragraph}
+                  </p>
+                ))}
+
+                <h2 className="text-[#110d34] mt-0 mb-[10px] text-[21px] md:text-[25px] lg:text-[28px] font-bold leading-[1.1em]">
+                  {TermsAndConditionsContent.subTitle2}
+                </h2>
+
+                {TermsAndConditionsContent.content2.map((paragraph, index) => (
+                  <p
+                    key={index}
+                    className="text-[#0a1015] text-[16px] leading-[1.5] font-normal mb-[30px]"
+                  >
+                    {paragraph}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 }
