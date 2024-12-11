ce60cfc076d7d6d491cec73f7b94f4cd1c025883
thanhtu <73648084+thanhtu2014@users.noreply.github.com>
2024-12-11 23:19:23 +0700

    Implement Returns & Cancellation Policy and Terms & Conditions

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -42,6 +42,7 @@ export const HEADER = [
 export const PrivacyPolicyContent = {
   title: "Privacy Policy",
   subtitle: "Introduction to Privacy Policy",
+  subTitle2: "Information we collect and how we use it",
   content: [
     `Subject to these Terms of Use, Merchant Onboarding Policy, KYC Policy, Disputes and Grievance Redressal Policy and Privacy Policy (collectively, "POLICIES"), Vearn Tech offers the Services set forth in Schedule I herein.`,
     `Schedule I also contains descriptions and process flows of all Services that are offered to Users. These descriptions and process flows are set out in Part A to Part G in Schedule I.`,
@@ -50,14 +51,15 @@ export const PrivacyPolicyContent = {
     `You acknowledge and agree that if Vearn Tech disables access to your account, you may be prevented from accessing the Services, your account details or any files or other content which is contained in your account, and Vearn Tech shall intimate you regarding the same.`,
     `Vearn Tech reserves the right to delete your User information stored in your account including but not limited to all or any personal information or any sensitive personal data or information ("SPDI") stored in your User account. Alternately, a User may notify us if they do not wish that we retain or use the personal information or SPDI by contacting the Grievance Officer (as provided below). However, in such a case, we may not be able to provide you some or all of our Services.`,
     `You acknowledge and agree that while Vearn Tech may not currently have set a fixed upper limit on the number of transmissions you may send or receive through the Services, such fixed upper limits may be set by Vearn Tech at any time, solely at Vearn Tech's discretion.`,
-    `Information we collect and how we use it`,
+  ],
+  content2: [
     `Subject to these Terms of Use, Merchant Onboarding Policy, KYC Policy, Disputes and Grievance Redressal Policy and Privacy Policy (collectively, "POLICIES"), Vearn Tech offers the Services set forth in Schedule I herein.`,
     `Schedule I also contains descriptions and process flows of all Services that are offered to Users. These descriptions and process flows are set out in Part A to Part G in Schedule I.`,
     `Vearn Tech is constantly evolving in order to provide the best possible experience and information to its Users. You acknowledge and agree that the form and nature of the Services which Vearn Tech provides may change from time to time without any prior notice to you.`,
     `As part of this continuing process, you acknowledge and agree that Vearn Tech may stop (permanently or temporarily) providing the Services (or any features within the Services) to you or to Users generally at Vearn Tech's sole discretion, without any prior notice. You may stop using the Services at any point of time. You do not need to specifically inform Vearn Tech when you stop using the Services.`,
     `You acknowledge and agree that if Vearn Tech disables access to your account, you may be prevented from accessing the Services, your account details or any files or other content which is contained in your account, and Vearn Tech shall intimate you regarding the same.`,
     `Vearn Tech reserves the right to delete your User information stored in your account including but not limited to all or any personal information or any sensitive personal data or information ("SPDI") stored in your User account. Alternately, a User may notify us if they do not wish that we retain or use the personal information or SPDI by contacting the Grievance Officer (as provided below). However, in such a case, we may not be able to provide you some or all of our Services.`,
-    `You acknowledge and agree that while Vearn Tech may not currently have set a fixed upper limit on the number of transmissions you may send or receive through the Services, such fixed upper limits may be set by Vearn Tech at any time, solely at Vearn Tech's discretion.`, 
+    `You acknowledge and agree that while Vearn Tech may not currently have set a fixed upper limit on the number of transmissions you may send or receive through the Services, such fixed upper limits may be set by Vearn Tech at any time, solely at Vearn Tech's discretion.`,
   ],
 };
 
@@ -66,7 +68,7 @@ export const RefundPolicyContent = {
   content: [
     `Our primary goal is to ensure utmost customer satisfaction. Should you find our services unsatisfactory, we offer a refund upon genuine reasons proven after investigation. Please review the specifics of each deal before purchase, as they outline all service or product details.`,
     `Refund Policy`,
-    `Regarding refunds, we aim to craft suitable design concepts for our clients. However, if a client isn't entirely satisfied with our products, we offer a refund option. Refunds for credit card payments will be credited back to the original card used during the purchase, while payments made through payment gateways will be refunded to the same account.`, 
+    `Regarding refunds, we aim to craft suitable design concepts for our clients. However, if a client isn't entirely satisfied with our products, we offer a refund option. Refunds for credit card payments will be credited back to the original card used during the purchase, while payments made through payment gateways will be refunded to the same account.`,
   ],
 };
 
@@ -76,13 +78,38 @@ export const contactData = [
     title: "Team Support",
     description: "He moonlights difficult-engrossed, sportsmen.",
     contact: "example@gmail.com",
-    alt: "email"
+    alt: "email",
   },
   {
     icon: "/images/location_icon.svg",
     title: "Find Us",
     description: "Unaffected at ye of compliment alteration to.",
-    contact: "9/2478 S/F, OLD NO- 974 975, S/O SH. <br /> RAJINDER KUMAR JAIN, GALI NO 18, <br /> KAILASH NAGAR, DELHI, East Delhi, <br /> Delhi, 110031",
-    alt: "location"
-  }
+    contact:
+      "9/2478 S/F, OLD NO- 974 975, S/O SH. <br /> RAJINDER KUMAR JAIN, GALI NO 18, <br /> KAILASH NAGAR, DELHI, East Delhi, <br /> Delhi, 110031",
+    alt: "location",
+  },
 ];
+
+export const TermsAndConditionsContent = {
+  title: "Terms and Conditions",
+  subtitle: "Introduction and Terms of Use",
+  subTitle2: "Use of the Services by User",
+  content: [
+    `Subject to these Terms of Use, Merchant Onboarding Policy, KYC Policy, Disputes and Grievance Redressal Policy and Privacy Policy (collectively, "POLICIES"), Vearn Tech offers the Services set forth in Schedule I herein.`,
+    `Schedule I also contains descriptions and process flows of all Services that are offered to Users. These descriptions and process flows are set out in Part A to Part G in Schedule I.`,
+    `Vearn Tech is constantly evolving in order to provide the best possible experience and information to its Users. You acknowledge and agree that the form and nature of the Services which Vearn Tech provides may change from time to time without any prior notice to you.`,
+    `As part of this continuing process, you acknowledge and agree that Vearn Tech may stop (permanently or temporarily) providing the Services (or any features within the Services) to you or to Users generally at Vearn Tech's sole discretion, without any prior notice. You may stop using the Services at any point of time. You do not need to specifically inform Vearn Tech when you stop using the Services.`,
+    `You acknowledge and agree that if Vearn Tech disables access to your account, you may be prevented from accessing the Services, your account details or any files or other content which is contained in your account, and Vearn Tech shall intimate you regarding the same.`,
+    `Vearn Tech reserves the right to delete your User information stored in your account including but not limited to all or any personal information or any sensitive personal data or information ("SPDI") stored in your User account. Alternately, a User may notify us if they do not wish that we retain or use the personal information or SPDI by contacting the Grievance Officer (as provided below). However, in such a case, we may not be able to provide you some or all of our Services.`,
+    `You acknowledge and agree that while Vearn Tech may not currently have set a fixed upper limit on the number of transmissions you may send or receive through the Services, such fixed upper limits may be set by Vearn Tech at any time, solely at Vearn Tech's discretion.`,
+  ],
+  content2: [
+    `Subject to these Terms of Use, Merchant Onboarding Policy, KYC Policy, Disputes and Grievance Redressal Policy and Privacy Policy (collectively, "POLICIES"), Vearn Tech offers the Services set forth in Schedule I herein.`,
+    `Schedule I also contains descriptions and process flows of all Services that are offered to Users. These descriptions and process flows are set out in Part A to Part G in Schedule I.`,
+    `Vearn Tech is constantly evolving in order to provide the best possible experience and information to its Users. You acknowledge and agree that the form and nature of the Services which Vearn Tech provides may change from time to time without any prior notice to you.`,
+    `As part of this continuing process, you acknowledge and agree that Vearn Tech may stop (permanently or temporarily) providing the Services (or any features within the Services) to you or to Users generally at Vearn Tech's sole discretion, without any prior notice. You may stop using the Services at any point of time. You do not need to specifically inform Vearn Tech when you stop using the Services.`,
+    `You acknowledge and agree that if Vearn Tech disables access to your account, you may be prevented from accessing the Services, your account details or any files or other content which is contained in your account, and Vearn Tech shall intimate you regarding the same.`,
+    `Vearn Tech reserves the right to delete your User information stored in your account including but not limited to all or any personal information or any sensitive personal data or information ("SPDI") stored in your User account. Alternately, a User may notify us if they do not wish that we retain or use the personal information or SPDI by contacting the Grievance Officer (as provided below). However, in such a case, we may not be able to provide you some or all of our Services.`,
+    `You acknowledge and agree that while Vearn Tech may not currently have set a fixed upper limit on the number of transmissions you may send or receive through the Services, such fixed upper limits may be set by Vearn Tech at any time, solely at Vearn Tech's discretion.`,
+  ],
+};
