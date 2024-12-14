00b2b94db6c73a4d6ce940e20b4f314e3bf59db1
thanhtu2014 <thanhtu2014td@gmail.com>
2024-12-14 17:33:29 +0700

    add seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,21 @@ export const metadata: Metadata = {
   title: "Vearn Tech: Seamless Online Payment Solutions in India | Vearn Tech",
   description:
     "Vearn Tech offers reliable online payment solutions for Indian customers, including mobile recharges and bill payments. Experience hassle-free transactions with our user-friendly platform and secure payment methods. Join us today for a seamless payment experience.",
+  openGraph: {
+    title:
+      "Vearn Tech: Seamless Online Payment Solutions in India | Vearn Tech",
+    description:
+      "Vearn Tech offers reliable online payment solutions for Indian customers, including mobile recharges and bill payments. Experience hassle-free transactions with our user-friendly platform and secure payment methods.",
+    images: [
+      {
+        url: "/public/seo.png",
+        alt: "Vearn Tech - Seamless Online Payment Solutions",
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+    url: "https://staticweb.online",
+  },
 };
 
 export default function RootLayout({
